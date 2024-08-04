422a7a1060b7db85e2ecd696dd63182f5679d233
OtmaneOm <ma.otmane@yahoo.com>
2024-08-04 18:09:48 +0100

    #first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <>
+          <Button className="p-2 bg-orange-400">Click me</Button>
+      </>
+  );
+}
